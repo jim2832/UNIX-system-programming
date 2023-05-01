743e0b3e982ab13ca2c6ea7c6f9cea47dcfe0d63
Jim <jim880705@gmail.com>
2023-05-01 09:22:26 +0800

    230501 update

---
+++ demo/UNIX_L9/broken.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int c;
+/* This is a test program.
+   The C /*..*/ operator
+   does not allow nesting.*/
+int main()
+{   scanf("%d",&c);
+    /* This runs for
+       values of c > 0*/
+    if (c>0)
+    {   printf("Yes\n");
+        while (c>0)
+        {   printf("%d\n",c--);
+}   }   }
